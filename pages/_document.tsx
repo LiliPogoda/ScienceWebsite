3f99d8c790ea3f413a13e02a7fe342bf1563fe9c
Jan Schering <j.schering@web.de>
2022-08-31 23:15:56 +0200

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-css-tags */
 /* eslint-disable @next/next/no-sync-scripts */
 import { Html, Head, Main, NextScript } from "next/document";
@@ -10,9 +11,8 @@ export default function Document() {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-83EMYWNDG1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
+          function gtag(){(dataLayer).push(arguments)}
+          gtag("js", new Date());
           gtag('config', 'G-83EMYWNDG1');
         </script>
         <link rel="stylesheet"
