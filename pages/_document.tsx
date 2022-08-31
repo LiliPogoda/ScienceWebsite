c3f9c8e1a3625539d96043d5fce83d9c898f8c12
Jan Schering <j.schering@web.de>
2022-08-31 23:13:16 +0200

    google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/*<!-- Google tag (gtag.js) -->*/}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-83EMYWNDG1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-83EMYWNDG1');
+        </script>
         <link rel="stylesheet"
         href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css"></link>
         <script src="https://3Dmol.csb.pitt.edu/build/3Dmol-min.js"></script>
